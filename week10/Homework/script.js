8c901b8ff0b1accd36097922767e79f9992ea2f7
awildelephant-bingao <65309437+awildelephant-bingao@users.noreply.github.com>
2020-11-15 20:19:19 -0800

    trying to make it work
    but it does not work

--- week10/Homework/script.js
+++ week10/Homework/script.js
@@ -1,13 +1,54 @@
-let firstElephantIMG = doctument.getElementById ("elephant1");
-let secondElephantIMG = doctument.getElementById ("elephant2");
-let thirdElephantIMG = doctument.getElementById ("elephant3");
-let fouthElephantIMG = doctument.getElementById ("elephant4");
-let bigIMG = document. getElementById ("bigImage");
+let firstElephantIMG = document.getElementById("elephant1");
+let secondElephantIMG = document.getElementById("elephant2");
+let thirdElephantIMG = document.getElementById("elephant3");
+let fouthElephantIMG = document.getElementById("elephant4");
+let bigIMG = document.getElementById("bigImage");
 
 
-let elephantFirstIMG = function(){
-    bigIMG.src ="images/elephant1.jpg";
+let swapFirstElephant = function () {
+    if (bigIMG.alt === "First elephant") {
+        bigIMG.src = "images/elephant2.jpg";
+        bigIMG.alt = "Second elephant";
+    } else {
+        bigIMG.src = "images/elephant1.jpg" 
+        bigIMG.alt = "First elephant";
+
+    }
+}
+
+let swapSecondElephant = function () {
+    if (bigIMG.alt === "First elephant") {
+        bigIMG.src = "images/elephant3.jpg";
+        bigIMG.alt = "Third elephant";
+    } else {
+        bigIMG.src = "images/elephant1.jpg" 
+        bigIMG.alt = "First elephant";
+    }
+}
+
+let swapThirdElephant = function () {
+    if (bigIMG.alt === "First elephant") {
+        bigIMG.src = "images/elephant3.jpg";
+        bigIMG.alt = "Third elephant";
+    } else {
+        bigIMG.src = "images/elephant1.jpg" 
+        bigIMG.alt = "First elephant";
+    }
+}
+
+let swapFourthElephant = function () {
+    if (bigIMG.alt === "First elephant") {
+        bigIMG.src = "images/elephant4.jpg";
+        bigIMG.alt = "Fourth elephant";
+    } else {
+        bigIMG.src = "images/elephant1.jpg" 
+        bigIMG.alt = "First elephant";
+    }
 }
 
 
-firstElephantIMG.addEvenListener ("click", elephantFirstIMG);
+
+firstElephantIMG.addEvenListener("click", swapFirstElephant);
+secondElephantIMG.addEvenListener("click", swapSecondElephant);
+thirdElephantIMG.addEvenListener("click", swapThirdElephant);
+fourthElephantIMG.addEvenListener("click", swapFourthElephant);
