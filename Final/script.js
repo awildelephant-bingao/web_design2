1cd4c671a8f07138182ca4d14aad2a21e5ee99c5
awildelephant-bingao <65309437+awildelephant-bingao@users.noreply.github.com>
2020-12-05 14:10:49 -0800

    work on the img change
    time change

---
+++ Final/script.js
@@ -0,0 +1,15 @@
+let carousel = document.getElementById("carousel");
+let carouselIMG = document.getElementById("carouselIMG");
+
+let imag1= "images/Home_1.jpg";
+let imag2= "images/Home_2.jpg";
+
+
+   let imgChange = function(){
+       carouselIMG.src = img2;
+
+   }
+
+   window.addEventListener("load",function(){
+       
+   })
