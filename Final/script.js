003503609312fbbbb2753a32c69bb4158e5cc867
awildelephant-bingao <65309437+awildelephant-bingao@users.noreply.github.com>
2020-12-06 13:23:45 -0800

    landing page almost done

--- Final/script.js
+++ Final/script.js
@@ -1,15 +1,32 @@
 let carousel = document.getElementById("carousel");
 let carouselIMG = document.getElementById("carouselIMG");
 
-let imag1= "images/Home_1.jpg";
-let imag2= "images/Home_2.jpg";
+
+
+let images = [
+    "Images/Home_1.jpg",
+    "Images/Home_2.jpg",
+    "Images/Home_3.jpg"
+]
+
+let counter = 0;
 
 
    let imgChange = function(){
-       carouselIMG.src = img2;
+       if(carouselIMG.alt === "First img 1"){
+           carouselIMG.src = images[1];
+        carouselIMG.alt = "First img 2";
+         }
+        else if (carouselIMG.alt === "First img 2"){
+            carouselIMG.src = images[2];
+         carouselIMG.alt = "First img 3";}
 
+         else if (carouselIMG.alt === "First img 3"){
+            carouselIMG.src = images[0];
+         carouselIMG.alt = "First img 1";}
    }
 
    window.addEventListener("load",function(){
+       setInterval(imgChange,2000);
        
    })
